69af6eba0057606ae81a6579b8368a4c782d5e4a
maksim stsefanenka <maksim.stsefanenka@mail.com>
2019-11-08 17:11:12 +0300

    add 2 HW 1 task

---
+++ HomeWork_2/script.js
@@ -0,0 +1,9 @@
+function function1(){
+    window.open("https://www.google.com");
+}
+
+function function2(){
+    document.getElementById('email').value = '';
+    document.getElementById('login').value = '';
+    document.getElementById('password').value = '';
+}
