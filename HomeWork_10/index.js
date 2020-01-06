6f8fd57e93470e87faea74099ecb7c4c104ff264
maksim stsefanenka <maksim.stsefanenka@mail.com>
2020-01-06 13:45:22 +0300

    grunt + uglify

--- HomeWork_10/index.js
+++ HomeWork_10/index.js
@@ -43,3 +43,24 @@ class ComputingServer extends Computer{
 var u1 = new Ultrabook("HP", "8", "256", "Intel", "Intel HD")
 
 console.log(u1.getManufacturer());
+
+//node index.js
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+  })
+
+app.put('/user', function (req, res) {
+    res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+    res.send('Got a DELETE request at /user')
+  })
